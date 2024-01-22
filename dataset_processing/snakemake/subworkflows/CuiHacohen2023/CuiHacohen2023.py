0a34b1778525ab303e6288c07d9d2925994515f7
stefanpeidli <stefanpeidli@gmail.com>
2024-01-22 11:28:00 +0100

    Update CuiHacohen: using their rds files, adding cytokine family annotations Thanks Tümay!

--- dataset_processing/snakemake/subworkflows/CuiHacohen2023/CuiHacohen2023.py
+++ dataset_processing/snakemake/subworkflows/CuiHacohen2023/CuiHacohen2023.py
@@ -39,5 +39,24 @@ adata.obs['organism'] = 'mouse'
 annotate_qc(adata, species='mouse')
 assert_annotations(adata)
 
+# Add cytokine family annotations, Thanks Tümay for the list!
+cytokine_families = {"IL-1": ["IL1a","IL1b", "IL1ra","IL18", "IL33", "IL36a", "IL36RA"],
+                     "Common beta chain/IL13/TSLP": ["IL2", "IL4", "IL13", "IL7", "TSLP", "IL9", "IL15", "IL21"],
+                     "Common beta chain": ["IL3", "IL5", "GM-CSF"],
+                     "IL-6/IL-12": ["IL6", "IL11", "IL27", "IL30", "IL31", "LIF", "OSM", "Cardiotrophin-1", "Neuropoietin", "IL12", "IL23", "IL-Y"],
+                     "IL-10": ["IL10", "IL19", "IL20", "IL22", "IL24"],
+                     "IL-17": ["IL17A","IL17B", "IL17C", "IL17D","IL17E", "IL17F"],
+                     "Interferon": ["IFNa1", "IFNb", "IFNe","IFNk", "IFNg","IFNl2"],
+                     "TNF": ["LTA1-B2", "LTA2-B1", "TNFa", "OX40L", "CD40L", "FasL", "CD27L", "CD30L", "41BBL","TRAIL","RANKL", "TWEAK", "APRIL", "BAFF", "LIGHT", "TL1A", "GITRL"],
+                     "Complement": ["C3a", "C5a"],
+                     "Growth factor": ["Flt3l", "IL34", "M-CSF", "G-CSF", "SCF", "EGF", "VEGF", "FGF-basic", "HGF", "IGF-I"],
+                     "Other": ["TGF-beta-1", "GDNF", "Persephin", "Prolactin", "Leptin", "Adiponectin","Resistin", "Noggin", "Decorin", "TPO"],
+                     "Control": ["control"]
+                    }
+adata.obs['cytokine_family'] = 'None'
+for key, values in cytokine_families.items():
+    for value in values:
+        adata.obs.loc[adata.obs.perturbation==value, 'cytokine_family'] = key
+
 adata.write(snakemake.output[0], compression='gzip')
 print('Done.')
